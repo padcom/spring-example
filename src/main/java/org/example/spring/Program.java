59422c98527ec22059fb017d913151920a1288d3
Maciej Hryniszak <maciej.hryniszak@lumesse.com>
2011-10-13 20:35:23 +0200

    Enabled bean retrieval

--- src/main/java/org/example/spring/Program.java
+++ src/main/java/org/example/spring/Program.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Program {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "beans.xml" });
-//		Action action = context.getBean(Action.class);
+		Action action = context.getBean(Action.class);
 		action.execute("Hello, world!");
 	}
 }
